9ae357aa376306bcabd71f1da0fef4080a676c00
Evan Losh <evand.losh@gmail.com>
2023-12-14 12:47:55 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    #0,1,1,2,3,5
+    if length == 0:
+        print([])
+        return None
+    elif length == 1:
+        print([0])
+        return None
+    elif length >= 2:
+        fib = [0,1]
+        for i in range(length-2):
+             fib.append(fib[i] + fib[i+1])
+        print(fib)
+    return None
